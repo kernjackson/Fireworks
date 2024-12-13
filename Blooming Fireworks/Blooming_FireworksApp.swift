6a2a44eeb46d06e96aaa5743076fe8bf43d06f5a
Kern Jackson <kernjackson@gmail.com>
2024-12-13 10:33:27 -0800

    Initial Commit

---
+++ Blooming Fireworks/Blooming_FireworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blooming_FireworksApp.swift
+//  Blooming Fireworks
+//
+//  Created by Kern Jackson on 12/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Blooming_FireworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
